797f07b4db583d7bd7224a3a0856d66288be4341
Maria Kolyvaki <maria.kolyv@hotmail.com>
2020-05-21 17:10:42 +0200

    Update GUI2048.java

--- GUI2048.java
+++ GUI2048.java
@@ -20,40 +20,50 @@ import java.awt.event.KeyListener;
  */
 public class GUI2048 extends JFrame {
 
-    private final Model model;
-    JPanel panelMain;
-    
+    //private final Model model;
+    private JPanel panelMain;
+    private JPanel panelGame;
     /**
      * Create a new GUI operating on the given Model.
      * @param model The model of this application
      */
-    public GUI2048(final Model model) {
-        this.model = model;
+    public GUI2048(/*final Model model*/) {
+        //this.model = model;
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        
         
         JPanel panelMain = new JPanel();
         panelMain.setBackground(Color.ORANGE);  
         frame.add(panelMain);
         
-        frame.pack();
+        BoxLayout box = new BoxLayout(panelMain, BoxLayout.Y_AXIS);
+        panelMain.setLayout(box);
         JPanel panelGame = new JPanel();
         panelMain.add(panelGame);
-        
+        panelMain.setPreferredSize(new Dimension(400, 400));
         JButton refresh = new JButton("↺");
         JButton right = new JButton("→");
         JButton left = new JButton("←");
         JButton up = new JButton("↑");
         JButton down = new JButton("↓");
-        panelGame.add(refresh);
-        panelGame.add(right);
-        panelGame.add(left);
-        panelGame.add(up);
-        panelGame.add(down);
-        pack();
-        frame.setVisible(true);
         
+        JPanel panelButtons = new JPanel();
+        panelMain.add(panelButtons);
+        panelButtons.setBackground(Color.ORANGE);  
+        panelButtons.add(refresh);
+        panelButtons.add(right);
+        panelButtons.add(left);
+        panelButtons.add(up);
+        panelButtons.add(down);
+        
+        panelGame.setLayout(new GridLayout(4, 4));
+        for (int i = 0; i < 16; i++) {
+            panelGame.add(new JLabel("check"));
+        }
+        frame.pack();
+        frame.setVisible(true);
+
         refresh.addActionListener(new RefreshListener() {
             public void actionPerformed(ActionEvent ev) {
                 System.out.println("Refresh clicked");
